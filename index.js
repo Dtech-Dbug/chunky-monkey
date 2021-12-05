cc302996e7092daa9284e04a6fbd2da18f693a8e
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-12-05 15:21:11 +0530

    update index.js = handled case when size param is 0

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function chunkyMonkey(arr, size) {
-  if (arr.length <= size) return [arr];
+  if (arr.length <= size || size <= 0) return [arr];
   else {
     return [arr.slice(0, size)].concat(chunkyMonkey(arr.slice(size), size));
   }
